ceac552401b92f599f33d38fdc4f1d2480c8fd51
Kiran Nambiar <nkirandroid@gmail.com>
2018-07-23 19:33:22 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<body>
 
-<?php
-// Set session variables
-$_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
-echo "Session variables are set.";
-?>
-
-</body>
-</html>
